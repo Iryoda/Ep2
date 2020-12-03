f5bf2f90a32d773d6f43cf8b3d610ca10fe8e5f1
ArtKanno <75317695+ArtKanno@users.noreply.github.com>
2020-12-02 21:51:20 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <stdexcept>
 #include <list>
-#include<string>
+#include <string>
 
 #include "PersistenciaDeRede.h"
 #include "Rede.h"
@@ -12,6 +12,7 @@
 #include "Navegador.h"
 
 using namespace std;
+
 
 void imprimirHospedeiro(Rede* rede) {
     int tamanhoList = rede->getHospedeiros()->size();
@@ -29,55 +30,80 @@ void imprimirHospedeiro(Rede* rede) {
             Navegador *navegador = dynamic_cast<Navegador*>(hospedeiro->getProcessos()->at(j));
 
             if(navegador != NULL)
-                cout << "\tNavegador " << navegador->getPorta();
+                cout << "\tNavegador " << navegador->getPorta() << endl;
             else
-                cout << "\tServidorWeb " << hospedeiro->getProcessos()->at(j)->getPorta();
+                cout << "\tServidorWeb " << hospedeiro->getProcessos()->at(j)->getPorta() << endl;
         }
         hospedeiros->pop_front();
     }
 }
 
 
-
-
-
 int main()
 {
     int opcao;
-    int sair;
+    int enderecoHospedeiro, portaNavegador, portaServidor, enderecoPagina, tempo, ttl;
+    bool sair = false;
     string arquivo;
 
     Rede* rede = new Rede();
 
     PersistenciaDeRede* pr = new PersistenciaDeRede();
 
-    cout << "Digite o nome do arquivo: " << endl;
+    cout << "Digite o nome do arquivo: ";
     cin >> arquivo;
 
-    pr->carregar(arquivo);
+     try {
+        rede = pr->carregar("arquivo.txt");
+    }catch (logic_error* e){
+        cout << e->what() << endl;
+    }catch (invalid_argument* e){
+        cout << e->what() << endl;
+    }
 
     while(!sair){
         cout << "Simulador de Rede"<< endl
         << "---"<< endl
-        <<"1) User Navegador" << endl
+        <<"1) Usar Navegador" << endl
         <<"2) Passar Tempo" << endl
         <<"3) Alterar TTL" << endl
         <<"4) Sair" << endl
-        <<"Escolha uma opcao: " << endl;
+        <<"Escolha uma opcao: ";
         cin >> opcao;
 
-
-        if(opcao == 1)
+        if(opcao == 1) {
             imprimirHospedeiro(rede);
+
+            cout << "Digite o endereco do hospedeiro : ";
+            cin >> enderecoHospedeiro;
+            cout << "Digite a porta do navegador: ";
+            cin >> portaNavegador;
+            cout << "Abrir pagina no endereco: ";
+            cin >> enderecoPagina;
+            cout << "Porta do Servidor Web: ";
+            cin >> portaServidor;
+
+            Hospedeiro *hospedeiro = dynamic_cast<Hospedeiro*>(rede->getNo(enderecoHospedeiro));
+            Navegador *navegador = dynamic_cast<Navegador*>(hospedeiro->getProcessos()->at(portaNavegador));
+            navegador->abrir(enderecoPagina, portaServidor);
+        }
+
+        if(opcao == 2) {
+            cout << "Quantidade de tempo: ";
+            cin >> tempo;
+            for(int i = 0; i < tempo; i++)
+                rede->passarTempo();
+        }
+
+        if(opcao == 3) {
+            cout << "TTL atual: " << ttl << endl;
+            cout << "Novo TTL: ";
+            cin >> ttl ;
+        }
+
+        if(opcao == 4)
+            sair = true;
     }
-
-
-
-
-
-
-
-
 
 
     /*try {
